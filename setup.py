e487a2cc2614d71f8cfa0ce5cb792063b9db0fb4
Mir Chen <or0986113303@gmail.com>
2018-07-19 17:56:44 +0800

    add setup.py for pip install
    Signed-off-by: Mir Chen <or0986113303@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = 'PyModuleTest',
+	packages = ['pkgA', 'pkgB'],
+	scripts = ['runner'],
+	version = '0.0.0.1',
+	description = 'My first project',
+	author = 'Mir',
+	author_email = 'or0986113303g@gmail.com',
+	keywords = ['Good Project'],
+	classifiers = [],
+)
